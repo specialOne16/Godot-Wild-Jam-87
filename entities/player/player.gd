596ddcd81bd50f00c02825787b2d37c001ba350a
Jun <jundi2303@gmail.com>
2025-11-16 03:31:06 +0700

    Basic player movement

---
+++ entities/player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+class_name Player
+
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
