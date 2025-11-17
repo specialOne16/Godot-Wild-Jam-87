a5baf3f5feaeabd049edef4d85b831281e39d694
priyanshudebnath <38852086+priyanshudebnath@users.noreply.github.com>
2025-11-17 09:55:04 +0530

    Removed predefined path

--- world/day_night_cycle/day_night_cycle.gd
+++ world/day_night_cycle/day_night_cycle.gd
@@ -7,29 +7,38 @@ class_name DayNightCycle
 @onready var dusk_timer: Timer = %DuskTimer
 @onready var night_timer: Timer = %NightTimer
 
+@export var morning_timer_time : float = 2
+@export var dusk_timer_time : float = 2
+@export var night_timer_time : float = 2
+
 func _ready() -> void:
 	dark_overlay.visible = false
 	dark_overlay.visible = false
+	timer_init()
 	toggle_morning_overlay()
 	#cycle_timer.start()
 	#cycle_timer.timeout.connect(toggle_morning_overlay)
 
+func timer_init():
+	morning_timer.wait_time = morning_timer_time
+	dusk_timer.wait_time = dusk_timer_time
+	night_timer.wait_time = night_timer_time
+	morning_timer.timeout.connect(toggle_dusk_overlay)
+	dusk_timer.timeout.connect(toggle_dark_overlay)
+	night_timer.timeout.connect(toggle_morning_overlay)
 
 func toggle_morning_overlay():
 	event_bus.enemy_spawner = false
 	dusk_overlay.visible = false
 	dark_overlay.visible = false
 	morning_timer.start()
-	morning_timer.timeout.connect(toggle_dusk_overlay)
 
 func toggle_dusk_overlay():
 	dusk_overlay.visible = not dusk_overlay.visible
 	dusk_timer.start()
-	dusk_timer.timeout.connect(toggle_dark_overlay)
 
 func toggle_dark_overlay():
 	dark_overlay.visible = not dark_overlay.visible
 	event_bus.enemy_spawner = true
 	night_timer.start()
-	night_timer.timeout.connect(toggle_morning_overlay)
 	
--- world/enemy_spawner/enemy_spawner.gd
+++ world/enemy_spawner/enemy_spawner.gd
@@ -1,12 +1,14 @@
 extends Node2D
 class_name EnemySpawner
 
+@export var spawned_move_speed : float = 100
+
 const BASE_ENEMY = preload("uid://devxha63j6w0k")
 
 @export var player: Player
 @onready var spawn_timer: Timer = $SpawnTimer
-@onready var path_1: Path2D = %Path1
-@onready var path_follow_1: PathFollow2D = %PathFollow1
+#@onready var path_1: Path2D = %Path1
+#@onready var path_follow_1: PathFollow2D = %PathFollow1
 
 
 func _ready() -> void:
@@ -16,14 +18,7 @@ func _ready() -> void:
 func spawn_enemy():
 	if event_bus.enemy_spawner == true:
 		var enemy: BaseEnemy = BASE_ENEMY.instantiate()
+		enemy.move_speed = spawned_move_speed
 		enemy.player = player
 		add_child(enemy)
-		# Create a follower for the path
-		var follower := PathFollow2D.new()
-		follower.rotates = true  # optional
 		
-		# Put the enemy inside the follower
-		follower.add_child(enemy)
-
-		# Add the follower to the Path2D
-		path_follow_1.add_child(follower)
