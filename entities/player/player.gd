c6d5d8bd97e0b0644277519d604a69b8c2daa680
Jebus-Crust <93569167+Jebus-Crust@users.noreply.github.com>
2025-11-17 03:39:58 -0500

    Added basic textures

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 class_name Player
-
+@onready var Anims = $"Player Anims/AnimationPlayer"
 @export var speed = 400
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	if input_direction:
+		Anims.play("Walk")
+	else:
+		Anims.play("Idle")
 
 func _physics_process(_delta):
 	get_input()
