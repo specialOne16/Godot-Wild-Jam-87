268699f3a44ba42e0840540ad89114ef82558ac2
Jun <49330357+specialOne16@users.noreply.github.com>
2025-11-20 11:16:40 +0700

    Crafting menu (#7)
    * add crafting menu
    
    * stone and steel resource and resource spawner
    
    * fix minor issue (zombie attack targets resource, using old way to update current wood, showing resource instance above wall)
    
    * second type building

--- globals/resource_manager.gd
+++ globals/resource_manager.gd
@@ -1,18 +1,26 @@
 extends Node
 
 signal wood_changed(new_value: int)
+signal stone_changed(new_value: int)
+signal steel_changed(new_value: int)
 
-var current_wood: int = 0
+var current_wood: int = 0 :
+	set(value):
+		current_wood = value
+		wood_changed.emit(value)
+
+var current_stone: int = 0 :
+	set(value):
+		current_stone = value
+		stone_changed.emit(value)
+
+var current_steel: int = 0 :
+	set(value):
+		current_steel = value
+		steel_changed.emit(value)
+
 const player_data_rm : PlayerData = preload("uid://b6otg17jy1a75") as PlayerData
 const zombie_data_rm : ZombieData = preload("uid://dd6b64bybbe8m") as ZombieData
 
 
 var enemy_list: Array
-
-func gain_wood(amount: int = 1):
-	current_wood += amount
-	wood_changed.emit(current_wood)
-
-func spend_wood(amount: int):
-	current_wood -= amount
-	wood_changed.emit(current_wood)
---
+++ world/building/building.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name Building
+
+@export var name: String
+
+@export var wood_cost: int
+@export var stone_cost: int
+@export var steel_cost: int
+
+@export var texture: Texture2D
+@export var scene: PackedScene
