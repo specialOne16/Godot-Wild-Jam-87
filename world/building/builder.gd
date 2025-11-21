b2157c136ce78b5ca2e16f4867c86fd65cb6d4cb
Jun <jundi2303@gmail.com>
2025-11-21 13:53:09 +0700

    cancel crafting and building texture

--- world/building/builder.gd
+++ world/building/builder.gd
@@ -1,6 +1,9 @@
 extends Node2D
 class_name Builder
 
+const perspective_offset = Vector2.DOWN * 8
+
+@export var world: Node2D
 @onready var building_guide: Sprite2D = $BuildingGuide
 var building_guide_data: Building
 
@@ -11,7 +14,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if building_guide.visible:
 		var mouse = get_local_mouse_position()
-		var asset_size = building_guide_data.texture.get_size()
+		var asset_size = building_guide_data.texture.get_size() / Vector2(building_guide_data.texture_frame_size)
 		building_guide.position = (mouse / asset_size).round() * asset_size
 		
 		if Input.is_action_just_pressed("confirm_build"):
@@ -20,13 +23,22 @@ func _process(_delta: float) -> void:
 			ResourceManager.current_steel -= building_guide_data.steel_cost
 			
 			var instance: Node2D = building_guide_data.scene.instantiate()
-			instance.position = building_guide.position
-			add_child(instance)
+			instance.position = building_guide.position + perspective_offset
+			world.add_child(instance)
 			
+			building_guide.visible = false
+			building_guide_data = null
+		
+		if Input.is_action_just_pressed("cancel_build"):
 			building_guide.visible = false
 			building_guide_data = null
 
 func start_building_guide(building: Building):
+	if not building: 
+		building_guide.visible = false
+		building_guide_data = null
+		return
+	
 	assert(building.wood_cost <= ResourceManager.current_wood)
 	assert(building.stone_cost <= ResourceManager.current_stone)
 	assert(building.steel_cost <= ResourceManager.current_steel)
@@ -34,4 +46,7 @@ func start_building_guide(building: Building):
 	building_guide_data = building
 	
 	building_guide.texture = building.texture
+	building_guide.hframes = building.texture_frame_size.x
+	building_guide.vframes = building.texture_frame_size.y
+	building_guide.frame = building.texture_frame
 	building_guide.visible = true
--- world/building/building.gd
+++ world/building/building.gd
@@ -7,5 +7,8 @@ class_name Building
 @export var stone_cost: int
 @export var steel_cost: int
 
+@export var recipe_texture: Texture2D
 @export var texture: Texture2D
+@export var texture_frame_size: Vector2i = Vector2i.ONE
+@export var texture_frame: int = 0
 @export var scene: PackedScene
