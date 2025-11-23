811a0c84132220cd400b5bce48543c9a7681d2c5
Jun <49330357+specialOne16@users.noreply.github.com>
2025-11-24 02:46:06 +0700

    main menu (#24)

--- Main Menu.gd
+++ Main Menu.gd
@@ -1,4 +1,16 @@
-extends Node2D
+extends CanvasLayer
+
 func _ready() -> void:
-	$AnimationPlayer.play("new_animation")
+	$VBoxContainer.visible = false
 	$AnimatedSprite2D.play("default")
+	
+	await $AnimatedSprite2D.animation_finished
+	$VBoxContainer.visible = true
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://world/world.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
